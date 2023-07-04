55a84c459f8835c7453103285678319e47d46886
saiteja72749 <saitejachintu821@gmail.com>
2023-07-04 23:37:26 +0530

    mrec

--- src/Components/Projects.js/GoogleMaps.js
+++ src/Components/Projects.js/GoogleMaps.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const GoogleMaps = () => {
   return (
     <div>GoogleMaps
-      <div><iframe width="80%" height="400" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=mlrit,hyderabad,India
+      <div><iframe width="80%" height="400" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=mrec,hyderabad,India
       
       &amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
     </div>
